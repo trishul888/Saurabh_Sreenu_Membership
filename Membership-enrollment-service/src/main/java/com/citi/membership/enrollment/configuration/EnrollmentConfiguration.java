9c053f98b0ee8e545f299ab02f385bad0c5d86cd
asus <asus@DESKTOP-5V94U0C-Saurabh>
2021-05-23 16:59:07 +0530

    Adding DataSOurce and JdbcTempate object in EnrollmentConfiguration file

--- Membership-enrollment-service/src/main/java/com/citi/membership/enrollment/configuration/EnrollmentConfiguration.java
+++ Membership-enrollment-service/src/main/java/com/citi/membership/enrollment/configuration/EnrollmentConfiguration.java
@@ -4,9 +4,13 @@
  */
 package com.citi.membership.enrollment.configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -25,5 +29,21 @@ public class EnrollmentConfiguration {
 	public RestTemplate restTempate() {
 		return new RestTemplate();
 	}
-
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource=new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:ORCL");
+		dataSource.setUsername("java");
+		dataSource.setPassword("saurabh");
+		
+		return dataSource();
+	}
+	
 }
