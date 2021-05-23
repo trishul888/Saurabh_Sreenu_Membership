81ee3145509a7c254fabd7d6276be5a25f428db6
asus <asus@DESKTOP-5V94U0C-Saurabh>
2021-05-23 21:21:08 +0530

    Adding Hibernate related sessionFactory creation object into EnrollmentConfiguration class

--- Membership-enrollment-service/src/main/java/com/citi/membership/enrollment/configuration/EnrollmentConfiguration.java
+++ Membership-enrollment-service/src/main/java/com/citi/membership/enrollment/configuration/EnrollmentConfiguration.java
@@ -4,13 +4,18 @@
  */
 package com.citi.membership.enrollment.configuration;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -24,17 +29,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.citi.membership.enrollment")
 public class EnrollmentConfiguration {
-	
+
 	@Bean
 	public RestTemplate restTempate() {
 		return new RestTemplate();
 	}
-	
+
 	@Bean
 	public JdbcTemplate jdbcTemplate() {
 		return new JdbcTemplate(dataSource());
 	}
-	
+
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource=new DriverManagerDataSource();
@@ -42,8 +47,30 @@ public class EnrollmentConfiguration {
 		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:ORCL");
 		dataSource.setUsername("java");
 		dataSource.setPassword("saurabh");
-		
+
 		return dataSource();
 	}
-	
+
+	private final Properties hibernateProperties() {
+		Properties hibernateProperties=new Properties();
+		hibernateProperties.setProperty("hibernate.dialect","org.hibernate.dialect.Oracle10gDialect");
+		hibernateProperties.setProperty("hibernate.show-sql","true");
+		hibernateProperties.setProperty("hibernate.format-sql","true");
+		return hibernateProperties;
+	}
+
+	private final SessionFactory sessionFactory() {
+		LocalSessionFactoryBean localSessionFactoryBean=new LocalSessionFactoryBean();
+		localSessionFactoryBean.setDataSource(dataSource());
+		localSessionFactoryBean.setHibernateProperties(hibernateProperties());
+		localSessionFactoryBean.setPackagesToScan("");
+		return (SessionFactory) localSessionFactoryBean;
+	}
+
+	@Bean
+	public HibernateTemplate hibernateTemplate() {
+		HibernateTemplate hibernateTemplate=new HibernateTemplate();
+		hibernateTemplate.setSessionFactory(sessionFactory());
+		return hibernateTemplate;
+	}
 }
