925baccd1d1e3267429334b47f69883afa6fa9f3
saurabh23patre93 <63465162+saurabh23patre93@users.noreply.github.com>
2021-05-30 13:28:59 +0530

    Membership enrollemnt simple jdbc implemented

--- Membership-enrollment-service/src/main/java/com/citi/membership/enrollment/dao/EnrollmentDaoImpl.java
+++ Membership-enrollment-service/src/main/java/com/citi/membership/enrollment/dao/EnrollmentDaoImpl.java
@@ -26,18 +26,17 @@ import com.citi.membership.enrollment.model.EnrollmentDaoResponse;
 public class EnrollmentDaoImpl implements EnrollmentDao {
 	private Logger logger=Logger.getLogger(EnrollmentDaoImpl.class);
 	public EnrollmentDaoResponse createEnrollment(EnrollmentDaoRequest enrollmentDaoRequest) throws BusinessException, SystemException  {
-		logger.debug("Enter into EnrollmentDaoImpl--start");
+		logger.debug("Enter into EnrollmentJdbcDaoImpl--start");
 		EnrollmentDaoResponse daoResponse=null;
 		try {
-			
+
 			//1.Get the request from service
 			//2.Prepare the request for db i.e. prepare the db queries
-		/*	Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection connection=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:ORCL","java","saurabh");
-			String sql = null;//"{call MRP_ENROLLMENT(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
-			//3.Call db and get the db response i.e. Resultset
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection=DriverManager.getConnection("jdbc:mysql:///saurabh_sreenu","root","Saurabh23patre@");
+			String sql="{call MRP_ENROLLMENT(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
 			CallableStatement cs=connection.prepareCall(sql);
-			
+
 			cs.registerOutParameter(13, Types.VARCHAR);
 			cs.registerOutParameter(14, Types.VARCHAR);
 			cs.registerOutParameter(15, Types.VARCHAR);
@@ -56,13 +55,14 @@ public class EnrollmentDaoImpl implements EnrollmentDao {
 			cs.setString(12, enrollmentDaoRequest.getEnrollmentDate());
 
 			boolean  b = cs.execute();
-			System.out.println("b is:"+b);
-			String dbRespCode = cs.getString(13);
-			String dbRespMsg = cs.getString(14);
-			*/
+			logger.debug("b is:"+b);
 			//4.Prepare the dao response
-			String dbResponseCode="000";
-			String dbResponseMsg=null;
+			String dbResponseCode = cs.getString(13);
+			String dbResponseMsg = cs.getString(14);
+
+			
+			//String dbResponseCode="000";
+			//String dbResponseMsg=null;
 			logger.info("Response code :"+dbResponseCode+" Response message "+dbResponseMsg);
 			if ("000".equals(dbResponseCode)) {
 				//TODO : replace the hard code values with database response
@@ -73,7 +73,7 @@ public class EnrollmentDaoImpl implements EnrollmentDao {
 				daoResponse.setResponseMsg("Success");
 				daoResponse.setDiscription("It is enrollment object call");
 			}else if( "100".equals(dbResponseCode) || "101".equals(dbResponseCode) || "1002".equals(dbResponseCode)){
-				
+
 				throw new BusinessException(dbResponseCode, dbResponseMsg);
 			} else {
 				throw new SystemException(dbResponseCode, dbResponseMsg);
@@ -89,7 +89,7 @@ public class EnrollmentDaoImpl implements EnrollmentDao {
 			//TODO : add error logs here
 			logger.error("System Exceptions ",se);
 			throw se;
-			
+
 		}catch (Exception e) {
 			// TODO Auto-generated catch block
 			logger.fatal("Unknown Error from database ",e);
